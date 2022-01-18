2eb6fe04ed8c1b4fe17244a527c8f03d848e1aa7
JooYoo <jooyoo@outlook.com>
2022-01-18 19:23:21 +0100

    feat: decrease, reset, increase count

--- UIkit-counter/ViewController.swift
+++ UIkit-counter/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // state
+    var count = 0
+    // UI
+    @IBOutlet weak var countLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // render UI after view load
+        renderUI()
     }
 
-
+    @IBAction func decreaseClicked(_ sender: Any) {
+        // update state
+        count -= 1
+        // update UI
+        renderUI()
+    }
+    @IBAction func resetClicked(_ sender: Any) {
+        count = 0
+        renderUI()
+    }
+    @IBAction func increaseClicked(_ sender: Any) {
+        count += 1
+        renderUI()
+    }
+    
+    func renderUI() {
+        countLabel.text = String(count)
+    }    
 }
 
